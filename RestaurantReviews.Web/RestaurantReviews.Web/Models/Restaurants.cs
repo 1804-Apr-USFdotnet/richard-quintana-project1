ecf3e2f8d71cb537b73b5556f01d1c13e0109792
Richard <quintanara@hotmail.com>
2018-05-09 12:40:36 -0700

    Anything

---
+++ RestaurantReviews.Web/RestaurantReviews.Web/Models/Restaurants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReviews.Web.Models
+{
+	public class Restaurants
+	{
+		int ID { get; set; }
+		[Required]
+		string Name { get; set; }
+		[Required]
+		string Address { get; set; }
+		[Required]
+		string StyleOfFood { get; set; }
+		[Required]
+		int Avg_Rating { get; set; }
+	}
+}
